faf3b600d45ff66e9278924f96fb678c5d209011
Your Name <you@example.com>
2025-07-01 12:50:32 +0530

    Make Choices as Whole
    Choices were displayed as single one by one with no relating to Question but Now I changed it by passing Classes of Choices to Question Registering with extra 3

--- polls/admin.py
+++ polls/admin.py
@@ -5,11 +5,23 @@ from .models import Question, Choice
 # class QuestionAdmin(admin.ModelAdmin):
 #     fields = ["pub_date", "question_text"]
 
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None, {"fields": ["question_text"]}),
+#         ("Date information", {"fields": ["pub_date"]}),
+#     ]
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["question_text"]}),
-        ("Date information", {"fields": ["pub_date"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
 
