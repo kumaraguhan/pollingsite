d4ba7ef28e64c557d5aa217d8354e774b6db0a28
Your Name <you@example.com>
2025-07-01 12:56:05 +0530

    Make Question List in more Cleaner Look using list_display of admin option

--- polls/admin.py
+++ polls/admin.py
@@ -23,6 +23,8 @@ class QuestionAdmin(admin.ModelAdmin):
     ]
     inlines = [ChoiceInline]
 
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
 admin.site.register(Question, QuestionAdmin)
 
 admin.site.register(Choice)
